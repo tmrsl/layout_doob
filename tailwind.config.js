5939618a93e307fd0d1e6c9a9b58d5495e9449f3
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-28 23:29:19 +0200

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", 'index.html'],
   theme: {
     extend: {
+      container: {
+        padding: '20px',
+      },
+
+      margin: {
+        '40': '40px',
+        '44': '44px',
+      },
+
       padding: {
         '4': '4px',
         '16': '16px',
@@ -13,18 +22,24 @@ module.exports = {
         '18': '18px',
         '36': '36px',
         '48': '48px',
+        '73': '73px',
       },
 
       backgroundImage: {
         'menu-open': "url('./../icons/menu-open.svg')",
         'menu-close': "url('./../icons/menu-close.svg')",
         'facebook': "url('./../icons/Facebook.svg')",
+        'facebook-hover': "url('./../icons/Facebook_hover.svg')",
         'twitter': "url('./../icons/Twitter.svg')",
-        'dribbble': "url('./../icons/Dribble.svg')",
+        'twitter-hover': "url('./../icons/Twitter_hover.svg')",
+        'dribble': "url('./../icons/Dribble.svg')",
+        'dribble-hover': "url('./../icons/Dribble_hover.svg')",
+        'slider-item': "url('./../icons/slider-item.png)"
       }
     },
     screens: {
-      'sm': '768px',
+      'sm': '769px',
+      'md': '1160px',
     }
   },
   plugins: [],
